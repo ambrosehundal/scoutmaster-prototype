8d373d23a0aa316bab9a30127c2dd807ad83b97e
Ambrose Hundal <amshunda@ucsc.edu>
2019-05-20 11:23:56 -0700

    user profile

---
+++ scoutmaster-prototype/app/controllers/profiles_controller.rb
@@ -0,0 +1,8 @@
+class ProfilesController < ApplicationController
+    #before anyone tries to access their profile, authenticate the user
+    before_action :authenticate_user!
+
+    def index
+        @user = current_user
+    end
+end
--- scoutmaster-prototype/db/schema.rb
+++ scoutmaster-prototype/db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_181045) do
+ActiveRecord::Schema.define(version: 2019_05_20_181935) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
